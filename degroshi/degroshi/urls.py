3df2df53aef7d76b7504420ed0984ca7d50d441a
DimaYermolenkoOut <dima.yermolenko@outlook.com>
2024-02-14 20:38:43 +0200

    проблема при додаванні оданкових витрат name error

--- degroshi/degroshi/urls.py
+++ degroshi/degroshi/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 from main import views
+from main.views import TotalExpensesView
 from main.views import CategotyAPIList, TotalExpensesView
 from django.urls import path
 
@@ -42,8 +43,15 @@ urlpatterns = [
     # path('api/v1/', CategotyAPIList.as_view()),
     # path('api/v1/<int:pk>/', CategotyAPIList.as_view()),
     path('', include(router.urls)),
-    path('total-expenses/', TotalExpensesView.as_view(), name='total_expenses'),
-
+    # path('total-expenses/', TotalExpensesView.as_view(), name='total_expenses'),
+    # path('expenses_by_day/', TotalExpensesView.as_view(), name='expenses_by_day'),
+    # path('expenses_by_user/', TotalExpensesView.as_view(), name='expenses_by_user'),
+    path('total-expenses/', views.TotalExpensesView.as_view(), {'get': 'get_total_expenses'}),
+    # URL для общих расходов
+    path('expenses-by-day/', views.TotalExpensesView.as_view(), {'get': 'get_expenses_by_day'},
+         name='expenses_by_day'),  # URL для расходов по дням
+    path('expenses-by-user/', views.TotalExpensesView.as_view(), {'get': 'get_expenses_by_user'},
+         name='expenses_by_user'),  # URL для расходов по пользователям
 ]
 urlpatterns +=router.urls
 urlpatterns += doc_urls
